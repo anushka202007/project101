8972b28f5358db878ed9ab1e620470f02802813d
anushka202007 <71169908+anushka202007@users.noreply.github.com>
2023-03-15 18:29:25 +0530

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,54 @@
+import random
+response="y"
+
+
+while response=="y":
+    no = random.randint(1,6)
+    if no==1:
+        print("[- - - - - ]")
+        print("[          ]")
+        print("[     0    ]")
+        print("[          ]")
+        print("[- - - - - ]")
+
+    if no==1:
+        print("[- - - - - ]")
+        print("[   0      ]")
+        print("[          ]")
+        print("[       0  ]")
+        print("[- - - - - ]")
+
+    if no==3:
+        print("[- - - - - ]")
+        print("[   0      ]")
+        print("[     0    ]")
+        print("[       0  ]")
+        print("[- - - - - ]")
+
+    if no==4:
+        print("[- - - - - ]")
+        print("[  0    0  ]")
+        print("[          ]")
+        print("[  0    0  ]")
+        print("[- - - - - ]")
+
+    if no==5:
+        print("[- - - - - ]")
+        print("[  0    0  ]")
+        print("[    0     ]")
+        print("[  0    0  ]")
+        print("[- - - - - ]")
+
+    if no==6:
+        print("[- - - - - ]")
+        print("[  0    0  ]")
+        print("[  0    0  ]")
+        print("[  0    0  ]")
+        print("[- - - - - ]")
+
+    response= input("Press y to roll a dice and n to exit")
+    print("\n")
+
+
+
+
